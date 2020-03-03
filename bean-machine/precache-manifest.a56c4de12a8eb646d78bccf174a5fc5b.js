be4aeb2ebb7c09a2d8c643a96a844f51743b1190
Francisco Ramos <jscriptcoder@gmail.com>
2020-03-04 00:00:53 +0100

    Added bean-machine

---
+++ bean-machine/precache-manifest.a56c4de12a8eb646d78bccf174a5fc5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac23a7651470eb3e093c090d2866537e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46988e82ef9fb933822b",
+    "url": "/static/css/2.03d7f783.chunk.css"
+  },
+  {
+    "revision": "471a592e61d9b336e698",
+    "url": "/static/css/main.dee8f8d6.chunk.css"
+  },
+  {
+    "revision": "46988e82ef9fb933822b",
+    "url": "/static/js/2.01ffab8a.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.01ffab8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "471a592e61d9b336e698",
+    "url": "/static/js/main.c9cfb423.chunk.js"
+  },
+  {
+    "revision": "de2520ba6809425867ae",
+    "url": "/static/js/runtime-main.d06d3672.js"
+  }
+]);
